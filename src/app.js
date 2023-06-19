76ab83f377ce50e35931f5b2030596f565e076ad
LucassMCoreea <lucasmauriciofc@gmail.com>
2023-06-19 20:23:33 -0300

    feat: updating categories with image

--- src/app.js
+++ src/app.js
@@ -14,8 +14,14 @@ class App {
 
   middlewares() {
     this.app.use(express.json())
+
     this.app.use(
       '/product-file',
+      express.static(resolve(__dirname, '..', 'uploads'))
+    )
+
+    this.app.use(
+      '/category-file',
       express.static(resolve(__dirname, '..', 'uploads'))
     )
   }
--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -22,6 +22,8 @@ class CategoryController {
 
     const { name } = request.body
 
+    const { filename: path } = request.file
+
     const categoryExist = await Category.findOne({
       where: { name },
     })
@@ -30,7 +32,7 @@ class CategoryController {
       return response.status(401).json({ error: 'Category aleredy exist' })
     }
 
-    const { id } = await Category.create({ name })
+    const { id } = await Category.create({ name, path })
 
     return response.json({ id, name })
   }
@@ -39,6 +41,45 @@ class CategoryController {
     const categories = await Category.findAll()
 
     return response.json(categories)
+  }
+
+  async update(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+    })
+
+    try {
+      await schema.validateSync(request.body, { abortEarly: false })
+    } catch (err) {
+      return response.status(400).json({ error: err.errors })
+    }
+
+    const { admin: isAdmin } = await User.findByPk(request.userId)
+
+    if (!isAdmin) {
+      return response.status(401).json()
+    }
+
+    const { name } = request.body
+
+    const { id } = request.params
+
+    const category = await Category.findByPk(id)
+
+    if (!category) {
+      return response
+        .status(401)
+        .json({ error: 'Make sure your category ID is correct' })
+    }
+
+    let path
+    if (request.file) {
+      path = request.file.filename
+    }
+
+    await Category.update({ name, path }, { where: { id } })
+
+    return response.status(200).json()
   }
 }
 
