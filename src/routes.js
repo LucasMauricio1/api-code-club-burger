1eecc7e6b8d25a210acbb2fbdeb175a0ef639313
LucassMCoreea <lucasmauriciofc@gmail.com>
2023-06-19 14:35:06 -0300

    feat: order controller created

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
 import ProductController from './app/controllers/ProductController'
 import CategoryController from './app/controllers/CategoryController'
+import OrderController from './app/controllers/OrderController'
 
 import authMiddleware from './app/middlewares/auth'
 
@@ -24,5 +25,7 @@ routes.get('/products', ProductController.index)
 
 routes.post('/categories', CategoryController.store)
 routes.get('/categories', CategoryController.index)
+
+routes.get('/orders', OrderController.store)
 
 export default routes
