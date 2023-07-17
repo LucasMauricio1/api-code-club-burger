a06cb8b90c067003cb88e8848289e9880d55de04
LucassMCoreea <lucasmauriciofc@gmail.com>
2023-07-17 20:24:36 -0300

    fix: deploy

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import routes from './routes'
 import { resolve } from 'path'
-import cors from 'cors'
 
 import './database'
 const express = require('express')
+const routes = require('./routes')
+const cors = require('cors')
 
 class App {
   constructor() {
